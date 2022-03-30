63e4237095b139af0f31e373cee999f209e93c61
AntoniaSperling <antonia.sperling@stundent.htw.-berlin.de>
2022-03-30 09:23:42 +0200

    Sachen aus dem Skript ausprobiert

---
+++ herumprobiert/first/src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: "about",
+    component: AboutComponent
+  },
+  {
+    path: "login",
+    component: LoginComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
